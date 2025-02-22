ebcfc10773716d9ea111054eeeb354ffd62d95a4
king.xy <king.xy@icloud.com>
2025-02-23 01:31:54 +0800

    Swift

---
+++ TextView.swift
@@ -0,0 +1,20 @@
+//
+//  TextView.swift
+//  Pods-XYNotesEditor_Example
+//
+//  Created by 王笑宇 on 2/22/25.
+//
+
+import UIKit
+
+class TextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ XYNotesEditor/Classes/LayoutManager.swift
@@ -0,0 +1,12 @@
+//
+//  LayoutManager.swift
+//  Pods
+//
+//  Created by 王笑宇 on 2/22/25.
+//
+
+import UIKit
+
+class LayoutManager: NSLayoutManager {
+
+}
